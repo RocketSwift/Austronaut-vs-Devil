8d4ea76a5d10dfea3af765ba3f9eebf482c949b6
Denys Zahorskyi <denys.zahorskyi@gmail.com>
2023-01-11 22:19:52 +0200

    astro walks and jumps

--- Austronaut vs Devil/AustronautSprite.swift
+++ Austronaut vs Devil/AustronautSprite.swift
@@ -10,22 +10,20 @@ import GameplayKit
 
 class AustronautSprite: SKSpriteNode {
     
+    let movingSpeed : CGFloat = 80
+    
     let walkTextures : [SKTexture] = [SKTexture(imageNamed: "astro_walk_one"),
                                       SKTexture(imageNamed: "astro_walk_two")]
-    
-//    public func  createAstroNode() -> SKSpriteNode {
-//        let asroTexture = SKTexture(imageNamed: "astro_stand")
-//        let astroNode = SKSpriteNode(texture: asroTexture, size: .init(width: 100, height: 100))
-//        return astroNode
-//    }
     
     init() {
         let texture = SKTexture(imageNamed: "astro_stand")
         super.init(texture: texture, color: .clear, size: .init(width: 100, height: 100))
         
-        self.physicsBody = .init(circleOfRadius: self.size.width / 2)
+        
+        self.physicsBody = SKPhysicsBody(texture: texture,
+                                                      size: CGSize(width: self.size.width,
+                                                                   height: self.size.height))
         self.physicsBody?.mass = 1
-        self.physicsBody?.isDynamic = false
         
     }
     
@@ -35,9 +33,40 @@ class AustronautSprite: SKSpriteNode {
     
     
     
-    func astroWalk(){
-        let walkAction = SKAction.animate(with: walkTextures, timePerFrame: 0.5)
-        run(.repeatForever(walkAction))
+    func astroWalk(direction: String){
+        
+        if self.action(forKey: "walk action") != nil{
+            self.removeAction(forKey: "walk action")
+        }
+        
+        if direction == "right"{
+            let walkAction = SKAction.group([
+                .repeat(.animate(with: walkTextures, timePerFrame: 0.4, resize: false, restore: true), count: 3),
+                .moveBy(x: movingSpeed, y: 0, duration: 2)
+            ])
+            run(walkAction, withKey: "walk action")
+            xScale = 1
+        } else {
+            let walkAction = SKAction.group([
+                .repeat(.animate(with: walkTextures, timePerFrame: 0.4, resize: false, restore: true), count: 3),
+                .moveBy(x: -movingSpeed, y: 0, duration: 2)
+            ])
+            run(walkAction, withKey: "walk action")
+            xScale = -1
+        }
+        
+    }
+    
+    func jump() {
+        self.physicsBody?.applyImpulse(.init(dx: 80, dy: 300))
+
+    }
+    
+    public func update() {
+        
+        if zRotation != 0 && action(forKey: "action_rotate") == nil {
+            run(SKAction.rotate(toAngle: 0, duration: 0.25), withKey: "action_rotate")
+        }
     }
     
 //    enum State {
--- Austronaut vs Devil/GameScene.swift
+++ Austronaut vs Devil/GameScene.swift
@@ -12,19 +12,64 @@ class GameScene: SKScene {
     
     var austronautSprite = AustronautSprite()
     
+    let cam = SKCameraNode()
+    
     override func didMove(to view: SKView) {
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -3.721)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.size = view.frame.size //self.frame.size //self.view.frame.size
+        background.position = .init(x: frame.minX, y: frame.minY)
+        background.zPosition = -1
+        addChild(background)
+        
+        let gestureRight = UISwipeGestureRecognizer(target: self, action: #selector(swiped(_:)))
+        gestureRight.direction = .right
+        view.addGestureRecognizer(gestureRight)
+        
+        let gestureUp = UISwipeGestureRecognizer(target: self, action: #selector(swiped(_:)))
+        gestureUp.direction = .up
+        view.addGestureRecognizer(gestureUp)
+        
+        let gestureLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped(_:)))
+        gestureLeft.direction = .left
+        view.addGestureRecognizer(gestureLeft)
         
         self.anchorPoint = .init(x: 0.5, y: 0.5)
         
-        austronautSprite.astroWalk()
         
         self.addChild(austronautSprite)
-        
-        
-        
+        createFloor()
         
     }
+
     
+@objc func swiped(_ sender: UISwipeGestureRecognizer) {
+       switch sender.direction {
+       case .left:
+           austronautSprite.astroWalk(direction: "left")
+       case .right:
+           austronautSprite.astroWalk(direction: "right")
+       case .up:
+           austronautSprite.jump()
+       default:
+           austronautSprite.jump()
+       }
+   }
+    
+    func createFloor(){
+        let floorNode = SKShapeNode(rectOf: .init(width: 10000, height: 3))
+        floorNode.physicsBody = .init(rectangleOf: .init(width: 10000, height: 1))
+        floorNode.physicsBody?.isDynamic = false
+        floorNode.position.x = frame.minX
+        floorNode.position.y = frame.minY + 20
+        self.addChild(floorNode)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        cam.position = austronautSprite.position
+    }
     
     
 }
